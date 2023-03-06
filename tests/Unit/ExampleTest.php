4b3d1688ef1a0fd2b1b19b88efe92fd185aa143e
Ss_Tr33s <24_de_7@protonmail.com>
2023-03-06 14:54:56 -0500

    otra prueba

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -12,6 +12,6 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         //correción
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 }
