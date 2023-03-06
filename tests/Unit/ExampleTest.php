0b6e8469ae6f7b9d8842f4972727013016282e16
Ss_Tr33s <24_de_7@protonmail.com>
2023-03-06 15:19:07 -0500

    uniendo con el master

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -31,7 +31,7 @@ class ExampleTest extends TestCase
 
         $result = $calculator->subtract(5, 3);
 
-        $this->assertEquals(4, $result);
+        $this->assertEquals(2, $result);
     }
 
     public function testMultiply()
