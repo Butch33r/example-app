36415c7e0535ae33829b80e16f1538c2cca948d9
Ss_Tr33s <24_de_7@protonmail.com>
2023-03-08 10:20:11 -0500

    prueba con postgress

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Calculator;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -18,6 +19,8 @@ class ExampleTest extends TestCase
 
     public function testAdd()
     {
+
+
         $calculator = new Calculator();
 
         $result = $calculator->add(2, 2);
@@ -49,6 +52,6 @@ class ExampleTest extends TestCase
 
         $result = $calculator->divide(10, 2);
 
-        $this->assertEquals(5, $result);
+        $this->assertEquals(7, $result);
     }
 }
